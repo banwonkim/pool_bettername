1dfe01bc9181860a38d94cd63511fd440798a98f
Nok <ileum37@gmail.com>
2026-03-18 10:40:14 +0900

    Update pool.h

--- pool.h
+++ pool.h
@@ -9,16 +9,16 @@ typedef struct poolFreed{
 } poolFreed;
 
 typedef struct {
-	uint32_t elementSize;
-	uint32_t blockSize;
-	uint32_t used;
-	int32_t block;
-	poolFreed *freed;
-	uint32_t blocksUsed;
-	uint8_t **blocks;
+	uint32_t elementSize; // memory size of one element
+	uint32_t elementCnt; // max count of element <- blockSize
+	uint32_t nowElementIdx; // index of using element <- used
+	int32_t nowBlockIdx; //index of using block / present block <- block
+	poolFreed *freeList; // freeList <- freed
+	uint32_t blockListCnt; // size of blocks, or count. <- blocksUsed
+	uint8_t **blockList; //blocks[0] , [1], [2]... <- blocks 
 } pool;
 
-void poolInitialize(pool *p, const uint32_t elementSize, const uint32_t blockSize);
+void poolInitialize(pool *p, const uint32_t elementSize, const uint32_t elementCnt);
 void poolFreePool(pool *p);
 
 #ifndef DISABLE_MEMORY_POOLING
@@ -26,7 +26,7 @@ void *poolMalloc(pool *p);
 void poolFree(pool *p, void *ptr);
 #else
 #include <stdlib.h>
-#define poolMalloc(p) malloc((p)->blockSize)
+#define poolMalloc(p) malloc((p)->elementCnt)
 #define poolFree(p, d) free(d)
 #endif
 void poolFreeAll(pool *p);
