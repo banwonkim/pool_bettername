ea56589b4c566d56642d6bbf5a2b14a2c9708191
Nok <ileum37@gmail.com>
2026-03-18 10:43:39 +0900

    Refactor memory pool structure and initialization

--- pool.c
+++ pool.c
@@ -7,75 +7,75 @@
 #define max(a,b) ((a)<(b)?(b):(a))
 #endif
 
-void poolInitialize(pool *p, const uint32_t elementSize, const uint32_t blockSize)
+void poolInitialize(pool *p, const uint32_t elementSize, const uint32_t elementCnt)
 {
 	uint32_t i;
 
 	p->elementSize = max(elementSize, sizeof(poolFreed));
-	p->blockSize = blockSize;
+	p->elementCnt = elementCnt;
 	
 	poolFreeAll(p);
 
-	p->blocksUsed = POOL_BLOCKS_INITIAL;
-	p->blocks = malloc(sizeof(uint8_t*)* p->blocksUsed);
+	p->blockListCnt = POOL_BLOCKS_INITIAL;
+	p->blockList = malloc(sizeof(uint8_t*)* p->blockListCnt);
 
-	for(i = 0; i < p->blocksUsed; ++i)
-		p->blocks[i] = NULL;
+	for(i = 0; i < p->blockListCnt; ++i)
+		p->blockList[i] = NULL;
 }
 
 void poolFreePool(pool *p)
 {
 	uint32_t i;
-	for(i = 0; i < p->blocksUsed; ++i) {
-		if(p->blocks[i] == NULL)
+	for(i = 0; i < p->blockListCnt; ++i) {
+		if(p->blockList[i] == NULL)
 			break;
 		else
-			free(p->blocks[i]);
+			free(p->blockList[i]);
 	}
 
-	free(p->blocks);
+	free(p->blockList);
 }
 
 #ifndef DISABLE_MEMORY_POOLING
 void *poolMalloc(pool *p)
 {
-	if(p->freed != NULL) {
-		void *recycle = p->freed;
-		p->freed = p->freed->nextFree;
+	if(p->freeList != NULL) {
+		void *recycle = p->freeList;
+		p->freeList = p->freeList->nextFree;
 		return recycle;
 	}
 
-	if(++p->used == p->blockSize) {
-		p->used = 0;
-		if(++p->block == (int32_t)p->blocksUsed) {
+	if(++p->nowElementIdx == p->elementCnt) {
+		p->nowElementIdx = 0;
+		if(++p->nowBlockIdx == (int32_t)p->blocksUsed) {
 			uint32_t i;
 
 			p->blocksUsed <<= 1;
-			p->blocks = realloc(p->blocks, sizeof(uint8_t*)* p->blocksUsed);
+			p->blockList = realloc(p->blockList, sizeof(uint8_t*)* p->blocksUsed);
 
 			for(i = p->blocksUsed >> 1; i < p->blocksUsed; ++i)
-				p->blocks[i] = NULL;
+				p->blockList[i] = NULL;
 		}
 
-		if(p->blocks[p->block] == NULL)
-			p->blocks[p->block] = malloc(p->elementSize * p->blockSize);
+		if(p->blockList[p->nowBlockIdx] == NULL)
+			p->blockList[p->nowBlockIdx] = malloc(p->elementSize * p->elementCnt);
 	}
 	
-	return p->blocks[p->block] + p->used * p->elementSize;
+	return p->blockList[p->nowBlockIdx] + p->nowElementIdx * p->elementSize;
 }
 
 void poolFree(pool *p, void *ptr)
 {
-	poolFreed *pFreed = p->freed;
+	poolFreed *pFreed = p->freeList;
 
-	p->freed = ptr;
-	p->freed->nextFree = pFreed;
+	p->freeList = ptr;
+	p->freeList->nextFree = pFreed;
 }
 #endif
 
 void poolFreeAll(pool *p)
 {
-	p->used = p->blockSize - 1;
-	p->block = -1;
-	p->freed = NULL;
+	p->nowElementIdx = p->elementCnt - 1;
+	p->nowBlockIdx = -1;
+	p->freeList = NULL;
 }
